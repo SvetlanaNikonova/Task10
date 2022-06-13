8f269d9b24c1d9833cd47f19c92a0e254246faf5
SvetlanaNikonova <SvetlanaNikonova@coherentsolutions.com>
2022-06-13 15:59:26 +0300

    fixes according to comments

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,11 +1,13 @@
 import org.junit.Ignore;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import parser.JsonParser;
 import parser.NoSuchFileException;
 import shop.Cart;
 import shop.RealItem;
 import shop.VirtualItem;
 import java.io.File;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,85 +16,88 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class Tests {
     @Test
-    public void TestJSonParserWriteToFile(){
+    public void testJSonParserWriteToFile(){
 
             JsonParser jsonParser = new JsonParser();
-            jsonParser.writeToFile(new Cart("Belekas"));
-            Path path = Paths.get("src//main//resources//Belekas.json");
-            assertTrue(Files.exists(path));
+            jsonParser.writeToFile(new Cart("CartItem"));
+            Path path = Paths.get("src/main/resources/CartItem.json");
+            assertTrue(String.format("File %s doesn't exist", path), Files.exists(path));
     }
 
     @Test
-    public void TestJSonParserReadFromFile(){
+    public void testJSonParserReadFromFile(){
         JsonParser jsonParser = new JsonParser();
-        assertNotNull(jsonParser.readFromFile(new File("src//main//resources//Belekas.json")));
+        assertNotNull(jsonParser.readFromFile(new File("src/main/resources/CartItem.json")));
     }
 
     @Test
-    public void TestRealItem(){
-        double expectedResult = 200;
+    public void testRealItem(){
+        BigDecimal expectedResult = new BigDecimal(200);
         RealItem realItem = new RealItem();
 
         realItem.setWeight(200);
-        assertEquals(realItem.getWeight(), expectedResult);
+        assertEquals(String.format("Expected weight was %s, but actual was %s", expectedResult, realItem.getWeight()),realItem.getWeight(), expectedResult);
     }
 
     @Test
-    public void TestVirtualItem(){
-        double expectedResult = 25.5;
+    public void testVirtualItem(){
+        BigDecimal expectedResult = new BigDecimal(25.5);
         VirtualItem virtualItem = new VirtualItem();
 
         virtualItem.setSizeOnDisk(25.5);
-        assertEquals(virtualItem.getSizeOnDisk(), expectedResult);
+        assertEquals(String.format("Expected size was %s, but actual was %s", expectedResult, virtualItem.getSizeOnDisk()), virtualItem.getSizeOnDisk(), expectedResult);
     }
 
     @Test
-    public void TestCart1(){
+    public void testCart1(){
         String expectedResult = "CartTest1";
-        assertEquals(GetCartName(),expectedResult);
+        assertEquals(String.format("Expected result was %s, but actual was %s", expectedResult, getCartName()), getCartName(), expectedResult);
     }
 
-    private String GetCartName(){
+    private String getCartName(){
         Cart cart = new Cart("CartTest1");
         return cart.getCartName();
     }
 
     @Test
-    public void TestCart2(){
-        double expectedPrice = 550.5 * 2 + (550.5 * 0.2) * 2;
-        assertEquals(GetCartTotalPrice(),expectedPrice);
+    public void testCart2(){
+        BigDecimal expectedPrice = new BigDecimal(550.5 * 2 + (550.5 * 0.2) * 2);
+        assertEquals(String.format("Expected price was %s, but actual was %s", expectedPrice, getCartTotalPrice("cart1", 550.5)), getCartTotalPrice("cart1", 550.5), expectedPrice);
     }
 
-    @Test(expectedExceptions = NullPointerException.class)
+    @Test
     public void testTestCart() {
-        JsonParser jsonParser = new JsonParser();
-        jsonParser.writeToFile(null);
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            JsonParser jsonParser = new JsonParser();
+            jsonParser.writeToFile(null);
+        });
     }
 
-    @Test(expectedExceptions = NoSuchFileException.class)
+    @Test
     public void testReadFromFile() {
-        JsonParser jsonParser = new JsonParser();
-        jsonParser.readFromFile(new File("testReadFromFile.txt"));
+        Assertions.assertThrows(NoSuchFileException.class, () -> {
+            JsonParser jsonParser = new JsonParser();
+            jsonParser.readFromFile(new File("testReadFromFile.txt"));
+        });
     }
 
-    private double GetCartTotalPrice(){
-        Cart cart = new Cart("CartTest2");
+    private double getCartTotalPrice(String cartName, double price ){
+        Cart cart = new Cart(cartName);
         RealItem realItem = new RealItem();
-        realItem.setPrice(550.5);
+        realItem.setPrice(price);
         cart.addRealItem(realItem);
         VirtualItem virtualItem = new VirtualItem();
-        virtualItem.setPrice(550.5);
+        virtualItem.setPrice(price);
         cart.addVirtualItem(virtualItem);
         return cart.getTotalPrice();
     }
 
     @Ignore
     @Test
-    public void TestCartGroupedAssertion(){
+    public void testCartGroupedAssertion(){
         assertAll("cart",
-                ()-> assertEquals(GetCartName(),"CartTest1"),
-                () -> assertEquals(GetCartTotalPrice(),(550.5 * 2 + (550.5 * 0.2) * 2))
+                ()-> assertEquals(getCartName(),"CartTest1"),
+                () -> assertEquals(getCartTotalPrice("Cart1", (550.5 * 2 + (550.5 * 0.2) * 2)), (550.5 * 2 + (550.5 * 0.2) * 2))
                 );
     }
-
 }
